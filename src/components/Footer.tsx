ea4132d02d9d46856fe6ac96cd525c774ef02f88
Erick Alvarez <erick505alvarez@gmail.com>
2025-02-05 20:55:04 -0800

    Fix footer font size. Add min-h-screen to Layout div

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <section className="w-full flex justify-center mt-10 mb-14">
-      <div className="PILL flex rounded-2xl overflow-hidden text-sm w-[300px] md:w-[600px] h-min">
+      <div className="PILL flex rounded-2xl overflow-hidden text w-[300px] md:w-[600px] h-min">
         <div className="hidden bg-white font-bold py-2 md:flex justify-center items-center w-[50%]">
           Let's Work Together
         </div>
