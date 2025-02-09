c901821c6d53ad2834a574373d20bcbe75e614a4
Erick Alvarez <erick505alvarez@gmail.com>
2025-02-08 20:11:06 -0800

    Optimize hero image and social icons

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-import erickPhoto from "../images/profile-photo-erick.png";
-import githubLogo from "../images/logos/github.png";
-import linkedinLogo from "../images/logos/linkedin.png";
-import tiktokLogo from "../images/logos/tiktok.png";
-import instagramLogo from "../images/logos/instagram.png";
+// import erickPhoto from "../images/profile-photo-erick.png";
+// import githubLogo from "../images/logos/github.png";
+// import linkedinLogo from "../images/logos/linkedin.png";
+// import tiktokLogo from "../images/logos/tiktok.png";
+// import instagramLogo from "../images/logos/instagram.png";
+
+import { StaticImage } from "gatsby-plugin-image";
 
 const HeroSection = () => {
   const logoClasses =
@@ -46,42 +48,56 @@ const HeroSection = () => {
       {/* ------------------ Photo Section start ------------------ */}
       <div className="PHOTO-CONTAINER border-red-800 relative translate-x-8 w-[200px] h-[200px] md:w-[350px] md:h-[350px]">
         <div className="overflow-hidden w-full h-full flex justify-center items-center rounded-full">
-          <img
+          {/* <img
             className="object-cover rounded-full w-[275px] h-[275px] md:w-[400px] md:h-[400px] relative bottom-4"
             src={erickPhoto}
             alt="profile photo"
+          /> */}
+          <StaticImage
+            className="object-cover rounded-full w-[275px] h-[275px] md:w-[400px] md:h-[400px] relative bottom-4"
+            src="../images/profile-photo-erick.png"
+            alt="profile photo"
+            placeholder="blurred"
           />
         </div>
         <a href="https://www.github.com/erick505alvarez" target="_blank">
-          <img
+          <StaticImage
             key="github"
-            className={`z-10 ${logoClasses} w-16 h-16 md:w-20 md:h-20 -left-10 md:-left-14 top-[60%] animate-float-github`}
-            src={githubLogo}
+            className={`z-10 ${logoClasses} w-16 h-16 md:w-20 md:h-20 -left-10 md:-left-14 top-[60%] rounded-full animate-float-github`}
+            // src={githubLogo}
+            src="../images/logos/github.png"
             alt="github logo"
+            placeholder="blurred"
           />
         </a>
         <a href="https://www.linkedin.com/in/erick505alvarez/" target="_blank">
-          <img
+          <StaticImage
             key="linkedin"
-            className={`z-8 ${logoClasses} w-20 h-20 md:w-32 md:h-32 -left-6 md:-left-8 top-[100%] md:top-[95%] animate-float-linkedin`}
-            src={linkedinLogo}
+            className={`z-8 ${logoClasses} w-20 h-20 md:w-32 md:h-32 -left-6 md:-left-8 top-[100%] md:top-[95%] rounded-full animate-float-linkedin`}
+            // src={linkedinLogo}
+            src="../images/logos/linkedin.png"
             alt="linkedin logo"
+            placeholder="blurred"
           />
         </a>
         <a href="https://www.tiktok.com/@prodbyseekay" target="_blank">
-          <img
+          <StaticImage
             key="tiktok"
-            className={`z-6 ${logoClasses} w-14 h-14 md:w-14 md:h-14 left-16 md:left-20 top-[118%] md:top-[110%] animate-float-tiktok`}
-            src={tiktokLogo}
+            className={`z-6 ${logoClasses} w-14 h-14 md:w-14 md:h-14 left-16 md:left-20 top-[118%] md:top-[110%] rounded-full animate-float-tiktok`}
+            // src={tiktokLogo}
+            src="../images/logos/tiktok.png"
             alt="tiktok logo"
+            placeholder="blurred"
           />
         </a>
         <a href="https://www.instagram.com/prodbyseekay/" target="_blank">
-          <img
+          <StaticImage
             key="instagram"
-            className={`z-4 ${logoClasses} w-16 h-16 md:w-20 md:h-20 left-40 md:left-44 top-[115%] md:top-[118%] animate-float-instagram`}
-            src={instagramLogo}
+            className={`z-4 ${logoClasses} w-16 h-16 md:w-20 md:h-20 left-40 md:left-44 top-[115%] md:top-[118%] rounded-full animate-float-instagram`}
+            // src={instagramLogo}
+            src="../images/logos/instagram.png"
             alt="instagram logo"
+            placeholder="blurred"
           />
         </a>
       </div>
