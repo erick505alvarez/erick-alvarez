1f7092907eddc78255360c1160295600a01f6054
Erick Alvarez <erick505alvarez@gmail.com>
2025-02-08 18:50:07 -0800

    Change project columns behavior

--- src/components/projects/ProjectsContainer.tsx
+++ src/components/projects/ProjectsContainer.tsx
@@ -92,7 +92,7 @@ function ProjectsContainer() {
 
   return (
     <section className="px-6 py-6">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {projects &&
           projects.map((project, index) => (
             <ProjectCard key={index} project={project} />
