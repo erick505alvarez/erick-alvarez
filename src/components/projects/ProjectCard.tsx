fbf93c6442f0700fa3d7fed54a3252bd2be36aad
Erick Alvarez <erick505alvarez@gmail.com>
2025-02-08 20:27:46 -0800

    Edit moleqlar background color to be #FDFDFD. Edit project header positions

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -15,16 +15,14 @@ function ProjectCard({ project }: ProjectCardProps) {
     <div className="w-80 h-80 rounded-xl mx-auto overflow-hidden relative">
       <div
         className={` bg-white w-full h-[80%] flex justify-center items-center ${
-          project.heading === "moleQLar" ? "bg-[#FCFCFD] px-4" : ""
+          project.heading === "moleQLar" ? "bg-[#FDFDFD] px-4" : ""
         }`}
       >
         {project.img}
       </div>
       <div className="px-4 flex flex-col justify-center w-full h-[20%] bg-primary text-white relative">
-        <div className="-translate-y-1">
-          <h1 className="">{project.heading}</h1>
-          <h2 className="">{project.subheading}</h2>
-        </div>
+        <h1 className="">{project.heading}</h1>
+        <h2 className="">{project.subheading}</h2>
       </div>
     </div>
   );
