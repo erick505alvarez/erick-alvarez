d132cc1fe5c9c2ab2590c90072c45ddab4dce907
Erick Alvarez <erick505alvarez@gmail.com>
2025-04-18 08:20:34 -0500

    Add timeline design

--- src/components/design-components/Timeline.tsx
+++ src/components/design-components/Timeline.tsx
@@ -20,10 +20,27 @@ const Timeline = () => {
     CONTACT: "before:bg-orange-500",
   };
 
+  const line_position = {
+    HERO: "top-20",
+    WASY: "top-20",
+    MOLEQLAR: "top-0",
+    BLACKROCK: "top-0",
+    CONTACT: "bottom-20",
+  };
+
+  const point_position = {
+    HERO: "before:top-0",
+    WASY: "before:top-0",
+    MOLEQLAR: "before:top-[30%]",
+    BLACKROCK: "before:top-[60%]",
+    CONTACT: "before:bottom-0 before:translate-y-0",
+  };
+
   return (
     <div
-      className={`TIMELINE ${point_color[currentPage]} w-1 absolute h-screen left-16 top-20
-    before:absolute before:top-0 before:left-0 before:content-[''] before:w-8 before:h-8 ${line_color[currentPage]}
+      className={`TIMELINE ${point_color[currentPage]} ${point_position[currentPage]} ${line_position[currentPage]}
+        w-1 absolute h-screen left-16 ${line_color[currentPage]}
+    before:absolute before:left-0 before:content-[''] before:w-8 before:h-8 
     before:rounded-full before:-translate-y-1/2 before:-translate-x-[calc(50%_-_0.125rem)]
     `}
     ></div>
